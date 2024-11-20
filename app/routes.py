5d939cd256c39aed0b9a6796f0fa1433f5509a4f
evggas <evgeniyagas2000@gmail.com>
2024-11-20 22:07:26 +0300

    form_post

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+try:
+    from flask import render_template, request, redirect, url_for
+    from app import app
+except ImportError as e:
+    print(f"Ошибка импорта: {e}")
+
+posts = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            posts.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+    return render_template('blog.html', posts=posts)
